0138f800054abe077454e35696f5932cce7feca5
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-11-22 21:39:52 +0100

    project2: Update wsgi.py

--- project_two/wsgi.py
+++ project_two/wsgi.py
@@ -19,7 +19,7 @@ def homepage():
 def upload_file():
 	f = request.files["file"]
 	f.save(f.filename)
-	ENDPOINT_URL_ = 'http://localstack:4566'
+	ENDPOINT_URL_ = 'http://localhost:4566'
 	s3client = boto3.client("s3", region_name="us-east-1", endpoint_url = ENDPOINT_URL_,aws_access_key_id="test",aws_secret_access_key="test")
 	file_filename = str(f.filename)
 	s3client.upload_file(file_filename, "terrabucket", file_filename)
@@ -28,15 +28,15 @@ def upload_file():
 
 @app.route('/view')
 def view():
-        ENDPOINT_URL_ = 'http://localhost:4566'
-        dclient = boto3.client("dynamodb", region_name="us-east-1", endpoint_url = ENDPOINT_URL_,aws_access_key_id="test",aws_secret_access_key="test")
-        jresponsearray = dclient.scan(TableName="terratable")
-        itemlist = jresponsearray["Items"]
-        jsonlist = []
-        for i in range(len(itemlist)):
-                jsonlist_ = itemlist[i]["filename"]["S"]
-                jsonlist.append(jsonlist_)
-        return render_template("view.html", jsonlist=jsonlist)
+    ENDPOINT_URL_ = 'http://localhost:4566'
+    dclient = boto3.client("dynamodb", region_name="us-east-1", endpoint_url = ENDPOINT_URL_,aws_access_key_id="test",aws_secret_access_key="test")
+    jresponsearray = dclient.scan(TableName="terratable")
+    itemlist = jresponsearray["Items"]
+    jsonlist = []
+    for i in range(len(itemlist)):
+        jsonlist_ = itemlist[i]["filename"]["S"]
+        jsonlist.append(jsonlist_)
+    return render_template("view.html", jsonlist=jsonlist)
 
 
 if __name__ == "__main__":
