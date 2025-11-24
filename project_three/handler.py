2f2a0fe896dc80054a09ab2ff993b03b0a97d3ae
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-11-24 11:22:30 +0100

    Project three: Update function

--- project_three/handler.py
+++ project_three/handler.py
@@ -5,7 +5,7 @@ import json
 sqs = boto3.client('sqs', endpoint_url='http://host.docker.internal:4566')
 
 # Configure the DynamoDB client to connect to LocalStack
-dynamodb = boto3.resource('dynamodb', endpoint_url='http://host.docker.internal:4566', region_name="us-east-1",
+dynamodb = boto3.resource('dynamodb', endpoint_url='http://home-localstack:4566', region_name="us-east-1",
                           aws_access_key_id="test",
                           aws_secret_access_key="test")
 
