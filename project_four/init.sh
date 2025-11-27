94e866378aeb2057e88de09aafb4a7cdcffcd0a7
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-11-27 09:45:41 +0100

    Project Four: Update init script

--- project_four/init.sh
+++ project_four/init.sh
@@ -6,24 +6,25 @@ echo "Initializing Project Four..."
 
 # Create Table in DynamoDB
 aws dynamodb create-table --table-name CoffeeShop --attribute-definitions AttributeName=coffeeId,AttributeType=S --key-schema AttributeName=coffeeId,KeyType=HASH --billing-mode PAY_PER_REQUEST
+
 # Insert Item into DynamoDB Table
 aws dynamodb put-item --table-name CoffeeShop --item '{"coffeeId": {"S": "C001"}, "name": {"S": "Espresso"}, "price": {"N": "4.50"}, "available": {"BOOL": true}}'
+
 # Create IAM Role
 aws iam create-role --role-name CoffeeShopRole --assume-role-policy-document file:///etc/localstack/init/ready.d/others/trust-policy.json
+
 # Create lambda function
-aws lambda create-function --function-name getCoffee --role arn:aws:iam::000000000000:role/CoffeeShopRole --runtime nodejs22.x --handler handler.getCoffee --zip-file fileb://get.zip --timeout 90
-# Update lambda function code
-aws lambda update-function-code --function-name  getCoffee --zip-file fileb:///root/localstack/project/localstack/project_four/lambda/get/get.zip
+aws lambda create-function --function-name getCoffee --role arn:aws:iam::000000000000:role/CoffeeShopRole --runtime nodejs22.x --handler handler.getCoffee --zip-file fileb:///etc/localstack/init/ready.d/others/get.zip --timeout 90
+# For Update lambda function code we can use the below command
+#aws lambda update-function-code --function-name  getCoffee --zip-file fileb:///etc/localstack/init/ready.d/others/get.zip
 
 # Create API Gateway REST API
-aws apigateway create-rest-api --name 'CoffeeShop'
-# Create route resources
-aws apigateway create-route --api-id CoffeeShop --route-key "GET /coffee" 
 rest_api_name="CoffeeShop"
 description="API for my Lambda function"
 rest_api_id=$(aws apigateway create-rest-api --name $rest_api_name --description "$description" --query 'id' --output text)
 resource_id=$(aws apigateway get-resources --rest-api-id $rest_api_id --query 'items[0].id' --output text)
 
+# Create route resources
 resource_coffee_id=$(aws apigateway create-resource --rest-api-id $rest_api_name --parent-id $resource_id --path-part "coffee" --query 'id' --output text)
 
 aws apigateway put-method --rest-api-id $rest_api_id --resource-id $resource_coffee_id --http-method GET --authorization-type NONE
