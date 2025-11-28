d1a15f92c17238cf981a848ed005d3babaebd2cd
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-11-28 10:12:16 +0100

    Project four: update script init

--- project_four/init.sh
+++ project_four/init.sh
@@ -15,6 +15,9 @@ aws iam create-role --role-name CoffeeShopRole --assume-role-policy-document fil
 
 # Create lambda function
 aws lambda create-function --function-name getCoffee --role arn:aws:iam::000000000000:role/CoffeeShopRole --runtime nodejs22.x --handler handler.getCoffee --zip-file fileb:///etc/localstack/init/ready.d/others/get.zip --timeout 90
+
+# Get the ARN of the created Lambda function
+LAMBDA_ARN=$(aws lambda get-function --function-name getCoffee --query 'Configuration.FunctionArn' --output text)
 # For Update lambda function code we can use the below command
 #aws lambda update-function-code --function-name  getCoffee --zip-file fileb:///etc/localstack/init/ready.d/others/get.zip
 
@@ -24,11 +27,45 @@ description="API for my Lambda function"
 rest_api_id=$(aws apigateway create-rest-api --name $rest_api_name --description "$description" --query 'id' --output text)
 resource_id=$(aws apigateway get-resources --rest-api-id $rest_api_id --query 'items[0].id' --output text)
 
-# Create route resources
-resource_coffee_id=$(aws apigateway create-resource --rest-api-id $rest_api_id --parent-id $resource_id --path-part "coffee" --query 'id' --output text)
+# Create route resources: /coffee and /coffee/id
+# Create route resources: /coffee
+resource_coffee_one=$(aws apigateway create-resource --rest-api-id $rest_api_id --parent-id $resource_id --path-part "coffee" --query 'id' --output text)
+resource_coffee_get_one=$(aws apigateway put-method --rest-api-id $rest_api_id --resource-id $resource_coffee_id --http-method GET --authorization-type NONE --request-parameters "method.request.path.coffee=true")
+aws apigateway put-integration --rest-api-id $rest_api_id --resource-id $resource_coffee_get_one --http-method GET --type AWS_PROXY --integration-http-method POST --uri "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:000000000000:function:getCoffee/invocations" --request-parameters "integration.request.path.coffee=method.request.path.coffee"
 
-aws apigateway put-method --rest-api-id $rest_api_id --resource-id $resource_coffee_id --http-method GET --authorization-type NONE
+# Create route resources: /coffee/id
+resource_coffee_two=$(aws apigateway create-resource --rest-api-id $rest_api_id --parent-id $resource_coffee_one --path-part "id" --query 'id' --output text)
+resource_coffee_get_two=$(aws apigateway put-method --rest-api-id $rest_api_id --resource-id $resource_coffee_two --http-method GET --authorization-type NONE --request-parameters "method.request.path.coffee.id=true")
+aws apigateway put-integration --rest-api-id $rest_api_id --resource-id $resource_coffee_get_two --http-method GET --type AWS_PROXY --integration-http-method POST --uri "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:000000000000:function:getCoffee/invocations" --request-parameters "integration.request.path.coffee.id=method.request.path.coffee.id"
 
-#awslocal apigateway put-integration --rest-api-id <YOUR_REST_API_ID> --resource-id <YOUR_RESOURCE_ID> --http-method GET --type HTTP --integration-http-method GET --uri "http://example.com/data" --request-templates '{"application/json": "{\"statusCode\": 200}"}'
+# Add permission for API Gateway to invoke the Lambda function
+aws lambda add-permission --function-name getCoffee --statement-id apigateway-test-invoke --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-1:*:$rest_api_id/*/*/coffee/*"
+# CHECKPOINT: Verify the policy added to the Lambda function
+aws lambda get-policy --function-name getCoffee
 
-#awslocal apigateway create-deployment --rest-api-id <YOUR_REST_API_ID> --stage-name "dev"
+# Deploy the API
+aws apigateway create-deployment --rest-api-id $rest_api_id --stage-name dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Manually adding the below line as it was getting cut off
+#aws apigateway put-integration --rest-api-id 2xch728gbn --resource-id fg9bcig4r4 --http-method GET --type AWS_PROXY --integration-http-method POST --uri "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:000000000000:function:getCoffee/invocations" --request-parameters "integration.request.path.coffee=method.request.path.coffee"
+#aws apigateway put-integration --rest-api-id 2xch728gbn --resource-id kbgopixbxo --http-method GET --type AWS_PROXY --integration-http-method POST --uri "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:000000000000:function:getCoffee/invocations" --request-parameters "integration.request.path.coffee.id=method.request.path.coffee.id"
+# Add permission for API Gateway to invoke the Lambda function
+#aws lambda add-permission --function-name getCoffee --statement-id apigateway-test-invoke --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-1:*:$rest_api_id/*/GET/coffee/*"
+# Manually adding the below line as it was getting cut off
+#aws lambda add-permission --function-name getCoffee --statement-id apigateway-test-invoke --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-1:*:2xch728gbn/*/GET/coffee/*"
+
+
